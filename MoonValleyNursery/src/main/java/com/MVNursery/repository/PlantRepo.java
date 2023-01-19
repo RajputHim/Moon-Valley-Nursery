e341cb6e30ed301091e00fbbd569c7a8ed1c3d33
vikas-9797 <vika.yadav222@gmail.com>
2023-01-19 14:59:17 +0530

    day-3

--- MoonValleyNursery/src/main/java/com/MVNursery/repository/PlantRepo.java
+++ MoonValleyNursery/src/main/java/com/MVNursery/repository/PlantRepo.java
@@ -3,9 +3,11 @@ package com.MVNursery.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.MVNursery.model.Plant;
 
+@Repository
 public interface PlantRepo extends JpaRepository<Plant, Integer> {
 
 	Optional<Plant> findByCommonName(String commonName);
