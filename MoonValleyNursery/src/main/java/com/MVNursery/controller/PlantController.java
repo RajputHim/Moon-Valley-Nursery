d5357ade187432296605a91750440f22eb816c9a
vikas-9797 <vika.yadav222@gmail.com>
2023-01-20 16:12:25 +0530

    day-4 git commit -mday-4

--- MoonValleyNursery/src/main/java/com/MVNursery/controller/PlantController.java
+++ MoonValleyNursery/src/main/java/com/MVNursery/controller/PlantController.java
@@ -1,11 +1,17 @@
 package com.MVNursery.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +32,39 @@ public class PlantController {
 		return new ResponseEntity<Plant>(savePlant, HttpStatus.CREATED);
 	}
 	
+	@PutMapping(value="")
+	public ResponseEntity<Plant> updatePlantHandler(@Valid @RequestBody Plant plant){
+		Plant updatePlant = pService.addPlant(plant);
+		return new ResponseEntity<Plant>(updatePlant, HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Plant> deletePlantHandler(@PathVariable Integer id){
+		Plant deletePlant = pService.deletePlant(id);
+		return new ResponseEntity<Plant>(deletePlant,HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Plant> viewPlantByIdHandler(@PathVariable Integer id){
+		Plant viewPlantById = pService.viewPlant(id);
+		return new ResponseEntity<Plant>(viewPlantById,HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/name/{commonName}")
+	public ResponseEntity<List<Plant>> viewPlantByCommonNameHandler(@PathVariable String commonName){
+		List<Plant> viewPlantByCoomonName = pService.viewPlantByName(commonName);
+		return new ResponseEntity<List<Plant>>(viewPlantByCoomonName,HttpStatus.OK);
+	}
+	
+	@GetMapping(value="")
+	public ResponseEntity<List<Plant>> viewAllPlantHandler(){
+		List<Plant> allPlant = pService.viewAllPlants();
+		return new ResponseEntity<List<Plant>>(allPlant,HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/type/{typeOfPlant}")
+	public ResponseEntity<List<Plant>> viewAllPlantByTypeOfPlantHandler(@PathVariable String typeOfPlant){
+		List<Plant> viewPlantByType = pService.viewAllPlants(typeOfPlant);
+		return new ResponseEntity<List<Plant>>(viewPlantByType,HttpStatus.OK);
+	}
 }
--- MoonValleyNursery/src/main/java/com/MVNursery/service/PlantServiceImpl.java
+++ MoonValleyNursery/src/main/java/com/MVNursery/service/PlantServiceImpl.java
@@ -1,5 +1,8 @@
 package com.MVNursery.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +10,12 @@ import com.MVNursery.exception.PlantException;
 import com.MVNursery.model.Plant;
 import com.MVNursery.repository.PlantRepo;
 
-import io.swagger.v3.oas.annotations.servers.Server;
-
 @Service
 public class PlantServiceImpl implements PlantService{
 
 	@Autowired
 	private PlantRepo pRepo;
+	
 	@Override
 	public Plant addPlant(Plant plant) throws PlantException {
 		Plant p = pRepo.save(plant);
@@ -22,6 +24,51 @@ public class PlantServiceImpl implements PlantService{
 		}else {
 			return p;
 		}
+	}
+	@Override
+	public Plant updatePlant(Plant plant) throws PlantException {
+		Optional<Plant> opt = pRepo.findById(plant.getPlantId());
+		if (opt.isPresent()) {
+			return pRepo.save(plant);
+		} else {
+			throw new PlantException("Not found....");
+		}
+	}
+	@Override
+	public Plant deletePlant(Integer id) throws PlantException {
+		Plant foundPlant = pRepo.findById(id).orElseThrow(() -> new PlantException("Plant does not exist"));
+		pRepo.delete(foundPlant);
+		return foundPlant;
+	}
+	@Override
+	public Plant viewPlant(Integer id) throws PlantException {
+		Plant foundPlant = pRepo.findById(id).orElseThrow(() -> new PlantException("Plant does not exist"));
+		return foundPlant;
+		
+	}
+	@Override
+	public List<Plant> viewPlantByName(String commonName) throws PlantException {
+		List<Plant> list = pRepo.findByCommonName(commonName);
+		if (list.isEmpty()) {
+			throw new PlantException("Plant does not exist with this Name");
+		} else {
+			return list;
+		}
+	}
+	@Override
+	public List<Plant> viewAllPlants() throws PlantException {
+		List<Plant> plantList = pRepo.findAll();
+		if (plantList.isEmpty())
+			throw new PlantException("No Plant Found...");
+		return plantList;
+		
+	}
+	@Override
+	public List<Plant> viewAllPlants(String typeOfPlant) throws PlantException {
+		List<Plant> plantList = pRepo.findByTypeOfPlant(typeOfPlant);
+		if (plantList.isEmpty())
+			throw new PlantException("No Plant Found...");
+		return plantList;
 		
 	}
 
